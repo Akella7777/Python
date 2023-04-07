167d341a7d5fe5f335755b244827023bdc6af14e
Mikhail Rudikov <hrom7777@mail.ru>
2023-04-08 03:08:27 +0500

    change Task06

--- Task06.py
+++ Task06.py
@@ -15,8 +15,21 @@ print("Введеите номер билета (целое шестизначн
 numberTicket = int(input())
 if 99999 < numberTicket < 1000000:
     firstThreeNumber = numberTicket // 1000
+    num1 = firstThreeNumber % 10
+    firstThreeNumber = firstThreeNumber // 10
+    num2 = firstThreeNumber % 10
+    firstThreeNumber = firstThreeNumber // 10
+    num3 = firstThreeNumber % 10
+    res_1 = num1 + num2 + num3
+    
     lastThreeNumber = numberTicket % 1000
-    if firstThreeNumber == lastThreeNumber:
+    num4 = lastThreeNumber % 10
+    lastThreeNumber = lastThreeNumber // 10
+    num5 = lastThreeNumber % 10
+    lastThreeNumber = lastThreeNumber // 10
+    num6 = lastThreeNumber % 10
+    res_2 = num4 + num5 + num6
+    if res_1 == res_2:
         print(f"билет с номером {numberTicket} – счастливый")
     else:
         print(f"билет с номером {numberTicket} – НЕ счастливый")

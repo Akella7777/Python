190dc293fe4ae2d4289c90ecd6db3d5fceaa29f8
Mikhail Rudikov <hrom7777@mail.ru>
2023-04-08 02:09:10 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Задача №1. Общее обсуждение
+# За день машина проезжает n километров.
+# Сколько дней нужно, чтобы проехать маршрут длиной m километров?
+# При решении этой задачи нельзя пользоваться условной инструкцией if и циклами.
+# Input:
+# n = 700
+# m = 750
+# Output:
+# 2
